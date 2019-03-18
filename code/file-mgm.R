eb8e0a52284e42168357f3cf3b109560cea6df17
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2019-03-19 01:46:37 +0800

    publish all

--- code/file-mgm.R
+++ code/file-mgm.R
@@ -3,6 +3,9 @@
 library(fs)
 library(tidyverse)
 
+
+# move -------------------------------------------------------------------
+
 dir_info(".", type = 'file', regexp = '\\.R?md$') %>% 
     filter(!path %>% str_detect('README|NEWS')) %>% 
     mutate(
@@ -11,6 +14,11 @@ dir_info(".", type = 'file', regexp = '\\.R?md$') %>%
 
 dir_delete("sql")
 
+
+# wflow -------------------------------------------------------------------
+
 wflow_status()
 
-wflow_git_commit("analysis/*",message = "transfer all into analysis")
+wflow_git_commit("analysis/*", message = "transfer all into analysis", all = TRUE)
+
+wflow_publish('analysis/*', message = "publish all", all = TRUE)
