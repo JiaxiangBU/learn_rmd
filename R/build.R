f5ac4ac98784e3ad3fa73e404b5fa61f437939ee
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2020-01-28 17:07:39 +0800

    build gitbook

---
+++ R/build.R
@@ -0,0 +1,4 @@
+rmarkdown::render("cookbook.Rmd")
+git2r::add(path = ".")
+git2r::commit(message = "build gitbook")
+git2r::push(name = 'origin', refspec = "refs/heads/master", cred = git2r::cred_token())
